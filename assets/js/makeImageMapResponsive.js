bab5d7ca9fa17bef4dd70efb89fda87e53598211
Nandy <ann19@NIST.GOV>
2025-01-08 21:26:01 -0500

    add initial working website files

---
+++ assets/js/makeImageMapResponsive.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', function (){
+
+	const img = document.querySelector('img[usemap]');
+	if (!img) return;
+
+	const imgmap = document.querySelector("map[name='sitemap']");
+	if (!imgmap) return;
+
+	const areas = imgmap.querySelectorAll('area');
+	
+	const origWidth = img.naturalWidth;
+	const origHeight = img.naturalHeight;
+	
+	function scaleMap(){
+
+		const curWidth = img.clientWidth;
+		const curHeight = img.clientHeight;
+		const widthratio = curWidth/origWidth;
+		const heightratio = curHeight/origHeight;
+		
+		areas.forEach(area => {
+			const origCoords = area.dataset.origCoords || area.getAttribute('coords');
+			area.dataset.origCoords = origCoords;
+			
+			const scaledCoords = origCoords.split(',').map((coord, index) => {
+				return index % 2 === 0
+					? Math.round(coord * widthratio)
+					: Math.round(coord * heightratio);
+			
+			});
+			area.setAttribute('coords', scaledCoords.join(','));
+
+
+		});
+
+	}
+	
+	scaleMap();
+	window.addEventListener('resize', scaleMap);
+});
