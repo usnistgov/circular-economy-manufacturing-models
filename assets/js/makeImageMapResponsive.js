ad09cc88af5f8c89ae409c6aa6557c14aeaba953
ananyan <nandy.ananya@gmail.com>
2025-01-31 13:28:11 -0500

    edit JS script to load earlier

--- assets/js/makeImageMapResponsive.js
+++ assets/js/makeImageMapResponsive.js
@@ -1,6 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function (){
-
 	const img = document.querySelector('img[usemap]');
 	if (!img) return;
 
@@ -13,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function (){
 	const origHeight = img.naturalHeight;
 	
 	function scaleMap(){
-
 		const curWidth = img.clientWidth;
 		const curHeight = img.clientHeight;
 		const widthratio = curWidth/origWidth;
@@ -30,19 +28,50 @@ document.addEventListener('DOMContentLoaded', function (){
 			
 			});
 			area.setAttribute('coords', scaledCoords.join(','));
-
-
 		});
 
 	}
 	
 	scaleMap();
-	window.addEventListener('load', scaleMap);
 	window.addEventListener('resize', scaleMap);
-	
-	document.onreadystatechange = function(){
-		if (document.readyState === 'complete'){
-			scaleMap();
-		}
-	}
 });
+
+document.onreadystatechange = function(){
+	if (document.readyState === 'complete'){
+		console.log("loading complete");
+		const img = document.querySelector('img[usemap]');
+		if (!img) return;
+
+		const imgmap = document.querySelector("map[name='sitemap']");
+		if (!imgmap) return;
+
+		const areas = imgmap.querySelectorAll('area');
+			
+		const origWidth = img.naturalWidth;
+		const origHeight = img.naturalHeight;
+			
+		function scaleMap(){
+			const curWidth = img.clientWidth;
+			const curHeight = img.clientHeight;
+			const widthratio = curWidth/origWidth;
+			const heightratio = curHeight/origHeight;
+				
+			areas.forEach(area => {
+				const origCoords = area.dataset.origCoords || area.getAttribute('coords');
+				area.dataset.origCoords = origCoords;
+					
+				const scaledCoords = origCoords.split(',').map((coord, index) => {
+					return index % 2 === 0
+						? Math.round(coord * widthratio)
+						: Math.round(coord * heightratio);
+					
+				});
+				area.setAttribute('coords', scaledCoords.join(','));
+			});
+			console.log('ran function');
+		}
+			
+		scaleMap();
+		window.addEventListener('resize', scaleMap);
+	}
+}
